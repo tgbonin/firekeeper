6e0dd02563ff4c513b619c6767fbc885835f21cc
Timothy Bonin (RIT Student) <txb2048@ad.rit.edu>
2016-04-26 16:35:30 -0400

    Created Saver and Loader functions which save the system time so we can calculate time difference between uses

---
+++ Assets/Scripts/GameSaveLoadManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class GameSaveLoadManager{
+
+    public static GameData savedData;
+
+    public static void Save()
+    {
+        GameData.CurrentGameData = new GameData();
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream fs = File.Create(Application.persistentDataPath + "/fireKeeperSave.save");
+        bf.Serialize(fs, GameData.CurrentGameData);
+        fs.Close();
+    }
+
+    public static void Load()
+    {
+        if(File.Exists(Application.persistentDataPath + "/fireKeeperSave.save"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream fs = File.Open(Application.persistentDataPath + "/fireKeeperSave.save", FileMode.Open);
+            GameData.CurrentGameData = (GameData)bf.Deserialize(fs);
+            fs.Close();
+        }
+        else
+        {
+            GameData.CurrentGameData = new GameData();
+        }
+    }
+}
---
+++ Assets/Scripts/TestGUI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class TestGUI : MonoBehaviour {
+
+	void OnGUI()
+    {
+        if(GUI.Button(new Rect(10, 10, 100, 20), "Save"))
+        {
+            Debug.Log("Save");
+            GameSaveLoadManager.Save();
+        }
+
+        if (GUI.Button(new Rect(10, 40, 100, 20), "Load"))
+        {
+            Debug.Log("Load");
+            Debug.Log(Application.persistentDataPath);
+            GameSaveLoadManager.Load();
+        }
+
+        GUIStyle centeredStyle = new GUIStyle(GUI.skin.textArea);
+        centeredStyle.alignment = TextAnchor.MiddleCenter;
+
+        GUI.TextArea(new Rect(10, 100, 160, 20), "Current Time: " + System.DateTime.Now.ToString("HH:mm:ss"), centeredStyle);
+
+        if (GameData.CurrentGameData != null)
+        {
+            GUI.TextArea(new Rect(10, 130, 160, 20), "Saved Time: " + GameData.CurrentGameData.saveTime.ToString("HH:mm:ss"), centeredStyle);
+        }
+    }
+}
